d3aae94ff77896c9f4ae760802c6aa85e1f1f3b6
Breckerdaproducer <whumobrianrinywe@gmail.com>
2025-03-12 10:39:38 +0100

    photographer

---
+++ click.js
@@ -0,0 +1,221 @@
+const one = document.querySelector(".one1");
+const one2 = document.querySelector(".one2");
+const one3 = document.querySelector(".one3");
+        const two = document.querySelector(".two1");
+        const two2 = document.querySelector(".two2");
+        const two3 = document.querySelector(".two3");
+        const sec2 = document.querySelector(".sec2");
+        const BtnOne = document.querySelector(".btn-one");
+        const BtnTwo = document.querySelector(".btn-two");
+        const Btnsec2 = document.querySelector(".btn-sec2");
+        const All = document.querySelector(".all");
+
+        All.addEventListener("click", function() {
+            one.classList.remove("display");
+            one2.classList.remove("display");
+            one3.classList.remove("display");
+            two.classList.remove("display");
+            two2.classList.remove("display");
+            two3.classList.remove("display");
+            sec2.classList.remove("display");
+          
+        });
+        BtnOne.addEventListener("click", function() {
+            one.classList.remove("display");
+            one2.classList.remove("display");
+            one3.classList.remove("display");
+            two.classList.add("display");
+            two2.classList.add("display");
+            two3.classList.add("display");
+            sec2.classList.add("display");
+        });
+        BtnTwo.addEventListener("click", function() {
+            one.classList.add("display");
+            one2.classList.add("display");
+            one3.classList.add("display");
+            two.classList.remove("display");
+            two2.classList.remove("display");
+            two3.classList.remove("display");
+            sec2.classList.add("display");
+        });
+        Btnsec2.addEventListener("click", function() {
+            one.classList.add("display");
+            one2.classList.add("display");
+            one3.classList.add("display");
+            two.classList.add("display");
+            two2.classList.add("display");
+            two3.classList.add("display");
+            sec2.classList.remove("display");
+        });
+
+        
+const one_1 = document.querySelector(".Architecture1");
+const one2_2 = document.querySelector(".Architecture2");
+const one3_3 = document.querySelector(".Architecture3");
+        const two_1 = document.querySelector(".Fashion1");
+        const two2_2 = document.querySelector(".Fashion2");
+        const two3_3 = document.querySelector(".Fashion3");
+        const sec3 = document.querySelector(".Food1");
+        const sec3_1 = document.querySelector(".Food2");
+        const sec3_2 = document.querySelector(".Food3");
+        const BtnOne1 = document.querySelector(".btn-one-1");
+        const BtnTwo1 = document.querySelector(".btn-two-1");
+        const Btnsec2_1 = document.querySelector(".btn-sec2-1");
+        const All1 = document.querySelector(".all2");
+
+        All1.addEventListener("click", function() {
+            one_1.classList.remove("display");
+            one2_2.classList.remove("display");
+            one3_3.classList.remove("display");
+            two_1.classList.remove("display");
+            two2_2.classList.remove("display");
+            two3_3.classList.remove("display");
+            sec3.classList.remove("display");
+            sec3_1.classList.remove("display");
+            sec3_2.classList.remove("display");
+            
+        });
+        BtnOne1.addEventListener("click", function() {
+            one_1.classList.remove("display");
+            one2_2.classList.remove("display");
+            one3_3.classList.remove("display");
+            two_1.classList.add("display");
+            two2_2.classList.add("display");
+            two3_3.classList.add("display");
+            sec3.classList.add("display");
+            sec3_1.classList.add("display");
+            sec3_2.classList.add("display");
+        });
+        BtnTwo1.addEventListener("click", function() {
+            one_1.classList.add("display");
+            one2_2.classList.add("display");
+            one3_3.classList.add("display");
+            two_1.classList.remove("display");
+            two2_2.classList.remove("display");
+            two3_3.classList.remove("display");
+            sec3_1.classList.add("display");
+            sec3_2.classList.add("display");
+            sec3.classList.add("display");
+        });
+        Btnsec2_1.addEventListener("click", function() {
+            one_1.classList.add("display");
+            one2_2.classList.add("display");
+            one3_3.classList.add("display");
+            two_1.classList.add("display");
+            two2_2.classList.add("display");
+            two3_3.classList.add("display");
+            sec3.classList.remove("display");
+            sec3_1.classList.remove("display");
+            sec3_2.classList.remove("display");
+        });
+
+const one_1_1 = document.querySelector(".Desert1");
+         const one2_a = document.querySelector(".Desert2");
+        const one3_a = document.querySelector(".Desert3");
+        const two_a = document.querySelector(".Mountain1");
+        const two2_a = document.querySelector(".Mountain2");
+        const two3_a = document.querySelector(".Mountain3");
+        const sec4 = document.querySelector(".Ocean1");
+        const sec4_1 = document.querySelector(".Ocean2");
+        const sec4_2 = document.querySelector(".Ocean3");
+        const BtnOne2 = document.querySelector(".btn-one-2");
+        const BtnTwo2 = document.querySelector(".btn-two-2");
+        const Btnsec2_2 = document.querySelector(".btn-sec2-2");
+        const All2 = document.querySelector(".all3");
+
+        All2.addEventListener("click", function() {
+            one_1_1.classList.remove("display");
+            one2_a.classList.remove("display");
+            one3_a.classList.remove("display");
+            two_a.classList.remove("display");
+            two2_a.classList.remove("display");
+            two3_a.classList.remove("display");
+            sec4.classList.remove("display");
+            sec4_1.classList.remove("display");
+            sec4_2.classList.remove("display");
+            
+        });
+        BtnOne2.addEventListener("click", function() {
+            one_1_1.classList.remove("display");
+            one2_a.classList.remove("display");
+            one3_a.classList.remove("display");
+            two_a.classList.add("display");
+            two2_a.classList.add("display");
+            two3_a.classList.add("display");
+            sec4.classList.add("display");
+            sec4_1.classList.add("display");
+            sec4_2.classList.add("display");
+        });
+        BtnTwo2.addEventListener("click", function() {
+            one_1_1.classList.add("display");
+            one2_a.classList.add("display");
+            one3_a.classList.add("display");
+            two_a.classList.remove("display");
+            two2_a.classList.remove("display");
+            two3_a.classList.remove("display");
+            sec4_1.classList.add("display");
+            sec4_2.classList.add("display");
+            sec4.classList.add("display");
+        });
+        Btnsec2_2.addEventListener("click", function() {
+            one_1_1.classList.add("display");
+            one2_a.classList.add("display");
+            one3_a.classList.add("display");
+            two_a.classList.add("display");
+            two2_a.classList.add("display");
+            two3_a.classList.add("display");
+            sec4.classList.remove("display");
+            sec4_1.classList.remove("display");
+            sec4_2.classList.remove("display");
+        });
+
+
+        const Classic = document.querySelector(".Classic");
+        const Classic2 = document.querySelector(".Classic2");
+        const Classic3 = document.querySelector(".Classic3");
+        const Fine = document.querySelector(".Fine");
+        const Fine2 = document.querySelector(".Fine2");
+        const Fine3 = document.querySelector(".Fine3");
+        const Monochrome = document.querySelector(".Monochrome");
+        const btn31 = document.querySelector(".btn31");
+        const btn32 = document.querySelector(".btn32");
+        const btn33 = document.querySelector(".btn33");
+        const all4 = document.querySelector(".all4");
+
+        all4.addEventListener("click", function() {
+            Classic.classList.remove("display");
+            Classic2.classList.remove("display");
+            Classic3.classList.remove("display");
+            Fine.classList.remove("display");
+            Fine2.classList.remove("display");
+            Fine3.classList.remove("display");
+            Monochrome.classList.remove("display");
+          
+        });
+        btn31.addEventListener("click", function() {
+             Classic.classList.remove("display");
+             Classic2.classList.remove("display");
+             Classic3.classList.remove("display");
+           Fine.classList.add("display");
+            Fine2.classList.add("display");
+            Fine3.classList.add("display");
+            Monochrome.classList.add("display");
+        });
+        btn32.addEventListener("click", function() {
+             Classic.classList.add("display");
+             Classic2.classList.add("display");
+             Classic3.classList.add("display");
+            Fine.classList.remove("display");
+            Fine2.classList.remove("display");
+            Fine3.classList.remove("display");
+            Monochrome.classList.add("display");
+        });
+        btn33.addEventListener("click", function() {
+             Classic.classList.add("display");
+             Classic2.classList.add("display");
+             Classic3.classList.add("display");
+            Fine.classList.add("display");
+            Fine2.classList.add("display");
+            Fine3.classList.add("display");
+            Monochrome.classList.remove("display");
+        });
